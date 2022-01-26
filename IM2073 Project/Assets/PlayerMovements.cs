e4b594632c468119c59374286eb7ecd103c5b80c
Zi Hui <46946696+Heng-Zi-Hui@users.noreply.github.com>
2022-01-27 00:15:53 +0800

    Updated player movements
    Fixed double jumping (only can jump when grounded),
    Added running and walking speeds!

---
+++ IM2073 Project/Assets/PlayerMovements.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    //VARIABLES
+    public float movespeed;
+    public float walkspeed;
+    public float runspeed;
+
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    public bool isGrounded;
+    public float groundCheckDistance;
+    public LayerMask groundMask;
+    public float gravity;
+
+    public float jumpHeight;
+    float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
+
+    //REFERENCES
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        moveDirection = new Vector3(horizontal, 0, vertical).normalized;
+        
+        if(isGrounded)
+        {
+            if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+            }
+
+            moveDirection *= movespeed;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        movespeed = walkspeed;
+    }
+
+    private void Run()
+    {
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        movespeed = runspeed;
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+    }
+}
