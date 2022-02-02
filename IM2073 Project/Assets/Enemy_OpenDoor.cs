9cba06d023536eab53989746a6c6e86b0cbf009b
ZhaoYiiii <zhaoyiwuu@gmail.com>
2022-02-03 00:46:00 +0800

    room 6 done

---
+++ IM2073 Project/Assets/Enemy_OpenDoor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_OpenDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    void OnTriggerEnter(Collider other){
+
+        if(other.CompareTag("Enemy")){
+            gameObject.GetComponent<AudioSource>().Play(); //play openDoor sound
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
